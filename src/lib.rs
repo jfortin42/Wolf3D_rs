fe327a67437273eac6e920b52eef4983ec1c595d
jfortin42 <djo_68@hotmail.fr>
2020-05-07 21:32:51 +0200

    [UODT] move necessary libs to the control module
    Co-authored-by: Francois Sidler <fsidler@student.42.fr>

--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,8 @@ pub extern crate serde_json;
 
 pub use clap::App;
 
-pub use sdl2::{
-    pixels::{ Color },
-    event::Event,
-    EventPump,
-    keyboard::Mod,
-    keyboard::Scancode,
-};
-
 pub use std::time::Duration;
 
-pub use std::rc::Rc;
-pub use std::cell::Cell;
 
 mod controls;
 pub use controls::controls::*;
